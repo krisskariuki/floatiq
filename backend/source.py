3f0bea6a42c4d8eb38aac99974abfdc2af3ba64f
krisskariuki <chri570ph3rk4riuki@gmail.com>
2025-01-19 20:35:06 +0300

    bug fixed and stability improvements

--- backend/source.py
+++ backend/source.py
@@ -4,6 +4,7 @@ from colorama import Fore
 import time
 import threading
 import json
+import os
 
 w=Fore.WHITE
 r=Fore.RED
@@ -19,6 +20,7 @@ class Navigator:
         self.phone=credentials[0]
         self.password=credentials[1]
 
+        self.fileName=None
         self.record={}
         self.series=[]
 
@@ -35,9 +37,19 @@ class Navigator:
         options.add_experimental_option('detach',True)
 
         self.driver=webdriver.Chrome(options=options,service=service)
+        self.driver.get(self.url)
+
+    def manage_data_backup(self,fileName):
+        file_id=1
+        if os.path.exists(f'raw_data/{fileName}.json'):
+            self.fileName=f'raw_data/{fileName}_{file_id}.json'
+            file_id+=1
+
+        else:
+            self.fileName=f'raw_data/{fileName}.json'
+        
 
     def navigate_to_game(self):
-        self.driver.get(self.url)
         print(f'{c}navigating to {self.url}...{w}')
 
         loginLink=WebDriverWait(self.driver,30).until(EC.element_to_be_clickable((By.XPATH,'//*[@class="login-link mozzart_ke"]')))
@@ -55,18 +67,24 @@ class Navigator:
 
         gameButton=WebDriverWait(self.driver,30).until(EC.element_to_be_clickable((By.XPATH,'//*[@alt="Aviator"]')))
         time.sleep(1)                                         
-        print(f'{c}navigating to game engine...{w}')
         gameButton.click()
+        print(f'{c}navigating to game engine...{w}')
+
     
-    def source(self):
-        self.navigate_to_game()
+    def start(self):
+        try:
+            self.navigate_to_game()
+
+        except (WebDriverException,StaleElementReferenceException,NoSuchWindowException,TimeoutException) as e:
+            print(f'{y} an error occured.Attempting to fix... {w}')
+            self.navigate_to_game()
 
         payoutsBlock=WebDriverWait(self.driver,30).until(EC.presence_of_element_located((By.XPATH,'//*[@class="payouts-block"]')))
         if payoutsBlock:
             print(f'{g}connected to game engine successfully{w}')
         
         previousMutipliers=None     
-        id=1
+        round_id=1
         std_dttm=None
         unix_dttm=None
         multiplier=None
@@ -76,6 +94,8 @@ class Navigator:
         RTP=None
 
         multiplier_series=[]
+
+        self.manage_data_backup('raw_data')
 
         try:
             while True:
@@ -98,18 +118,18 @@ class Navigator:
                         RTP=round((multiplier*probability),2)
                     
 
-                    data={'id':id,'std_time':std_dttm,'multiplier':multiplier,'probability':probability,'bets':bets,'payout':payout,'rtp':RTP,'unix_time':unix_dttm}
+                    data={'id':round_id,'std_time':std_dttm,'multiplier':multiplier,'probability':probability,'bets':bets,'payout':payout,'rtp':RTP,'unix_time':unix_dttm}
 
                     self.record=json.dumps(data,separators=(',',':'))
 
                     self.series.append(data)
 
-                    with open('db/raw_series.json','w') as file:
+                    with open(self.fileName,'w') as file:
                         fileSeries=json.dumps(self.series,indent=True,separators=(',',':'))
                         file.write(fileSeries)
 
                     print(f'{w}{data}\n{w}')
-                    id+=1
+                    round_id+=1
                 time.sleep(0.1)
 
         except (WebDriverException,StaleElementReferenceException,NoSuchWindowException,TimeoutException) as e:
@@ -117,4 +137,4 @@ class Navigator:
             self.navigate_to_game()
 
 mozzartGame=Navigator(('0113294793','Chri570ph3r.'),'https://www.mozzartbet.co.ke/en#/casino')
-mozzartGame.source()
+mozzartGame.start()
