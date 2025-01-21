5d69859b3136a9a67d5f83d29976ac5538451806
krisskariuki <chri570ph3rk4riuki@gmail.com>
2025-01-21 15:56:48 +0300

    added error handling mechanisms for if an error occurs in the automation process

--- backend/source.py
+++ backend/source.py
@@ -1,15 +1,14 @@
 from selenium_imports import *
 from datetime import datetime
 from colorama import Fore
-import time
 import threading
+import logging
 import json
+import time
 import os
 
 w=Fore.WHITE
-r=Fore.RED
 g=Fore.GREEN
-b=Fore.BLUE
 y=Fore.YELLOW
 c=Fore.CYAN
 m=Fore.MAGENTA
@@ -20,7 +19,14 @@ class Navigator:
         self.phone=credentials[0]
         self.password=credentials[1]
 
+        self.balance=0
+        self.stake=1
+
         self.fileName=None
+        self.target_multiplier=2.0
+        self.target_multiplier_rtp=0
+        self.selection_multipliers_rtps=[]
+        self.selection_multipliers=[1.2,1.5,1.75,2.0,2.25,2.5,3.0,3.5,4.0,5.0,6.0,7.0,8.0,9.0,10.0]
         self.record={}
         self.series=[]
 
@@ -35,9 +41,10 @@ class Navigator:
         for arg in args:
             options.add_argument(arg)
         options.add_experimental_option('detach',True)
-
+        
         self.driver=webdriver.Chrome(options=options,service=service)
-        self.driver.get(self.url)
+
+        logging.basicConfig(level=logging.INFO,format='%(asctime)s | %(levelname)s | %(message)s',datefmt='%Y-%m-%d %H:%M:%S')
 
     def manage_data_backup(self,fileName):
         file_id=1
@@ -48,54 +55,83 @@ class Navigator:
         else:
             self.fileName=f'raw_data/{fileName}.json'
         
+    def predict_multiplier(self,multiplier_choices,multipliers_array):
+        rtp=0
+        rtp_series=[]
+
+        if len(multipliers_array) > 0:
+            for target_multiplier in multiplier_choices:
+                count=sum(1 for num in multipliers_array if num > target_multiplier)
+
+                probability=round((count/len(multipliers_array)),2)
+                rtp=target_multiplier*probability
+
+                rtp_series.append(round(rtp,2))
+                self.selection_multipliers_rtps.append([target_multiplier,round(rtp,4)])
+
+                selected_rtp_index=rtp_series.index(max(rtp_series))
+                self.target_multiplier=multiplier_choices[selected_rtp_index]
+                self.target_multiplier_rtp=rtp_series[selected_rtp_index]
+
+    def execute_trade(self,outcome_multiplier,predicted_multiplier):
+        if outcome_multiplier > predicted_multiplier:
+            self.balance+=(self.stake*predicted_multiplier)-self.stake
+        else:
+            self.balance-=self.stake
 
     def navigate_to_game(self):
-        print(f'{c}navigating to {self.url}...{w}')
 
-        loginLink=WebDriverWait(self.driver,30).until(EC.element_to_be_clickable((By.XPATH,'//*[@class="login-link mozzart_ke"]')))
-        print(f'{c}logging in...{w}')
-        loginLink.click()
-
-
-        phoneInput=WebDriverWait(self.driver,30).until(EC.presence_of_element_located((By.XPATH,'//*[@placeholder="Mobile number"]')))
-        print(f'{c}writing phone input...{w}')
-        phoneInput.send_keys(self.phone)
-
-        passwordInput=WebDriverWait(self.driver,30).until(EC.presence_of_element_located((By.XPATH,'//*[@placeholder="Password"]')))
-        print(f'{c}writing password input...{w}')
-        passwordInput.send_keys(self.password + Keys.RETURN)
-
-        gameButton=WebDriverWait(self.driver,30).until(EC.element_to_be_clickable((By.XPATH,'//*[@alt="Aviator"]')))
-        time.sleep(1)                                         
-        gameButton.click()
-        print(f'{c}navigating to game engine...{w}')
-
-    
-    def start(self):
         try:
+            self.driver.get(self.url)
+            logging.info(f'{c}navigating to {self.url}...{w}')
+
+            loginLink=WebDriverWait(self.driver,30).until(EC.element_to_be_clickable((By.XPATH,'//*[@class="login-link mozzart_ke"]')))
+            logging.info(f'{c}logging in...{w}')
+            loginLink.click()
+
+
+            phoneInput=WebDriverWait(self.driver,30).until(EC.presence_of_element_located((By.XPATH,'//*[@placeholder="Mobile number"]')))
+            logging.info(f'{c}writing phone input...{w}')
+            phoneInput.send_keys(self.phone)
+
+            passwordInput=WebDriverWait(self.driver,30).until(EC.presence_of_element_located((By.XPATH,'//*[@placeholder="Password"]')))
+            logging.info(f'{c}writing password input...{w}')
+            passwordInput.send_keys(self.password + Keys.RETURN)
+
+            gameButton=WebDriverWait(self.driver,30).until(EC.element_to_be_clickable((By.XPATH,'//*[@alt="Aviator"]')))
+            time.sleep(1)                                         
+            gameButton.click()
+            logging.info(f'{c}navigating to game engine...{w}')
+
+        except (AttributeError,WebDriverException,StaleElementReferenceException,NoSuchWindowException,TimeoutException) as e:
+
+            if 'no such window' in str(e):
+                self.driver.quit()
+                logging.error(f'{y}navigation procedure error - no such window exception\n{c}Attempting to fix...{w}')
+                self.__init__((self.phone,self.password),self.url)
+                self.navigate_to_game()
+
+            self.driver.quit()
+            logging.error(f'{y}navigation procedure error- {e}\n{c}Attempting to fix...{w}')
+            self.__init__((self.phone,self.password),self.url)
             self.navigate_to_game()
 
-        except (WebDriverException,StaleElementReferenceException,NoSuchWindowException,TimeoutException) as e:
-            print(f'{y} an error occured.Attempting to fix... {w}')
-            self.navigate_to_game()
 
-        payoutsBlock=WebDriverWait(self.driver,30).until(EC.presence_of_element_located((By.XPATH,'//*[@class="payouts-block"]')))
-        if payoutsBlock:
-            print(f'{g}connected to game engine successfully{w}')
-        
+    def source(self):
         previousMutipliers=None     
         round_id=1
+        sliding_window=22
         std_dttm=None
-        unix_dttm=None
-        multiplier=None
-        bets=None
-        probability=None
-        payout=None
-        RTP=None
+        multiplier=0
+        bets=0
+        probability=0
+        payout=0
+        RTP=0
 
-        multiplier_series=[]
-
-        self.manage_data_backup('raw_data')
+               
+        payoutsBlock=WebDriverWait(self.driver,30).until(EC.presence_of_element_located((By.XPATH,'//*[@class="payouts-block"]')))
+        if payoutsBlock:
+            logging.info(f'{g}connected to game engine successfully{w}')
 
         try:
             while True:
@@ -107,21 +143,23 @@ class Navigator:
                     bets=int(self.driver.find_element(By.XPATH,'//*[@class="all-bets-block d-flex justify-content-between align-items-center px-2 pb-1"]').find_elements(By.XPATH,'.//div')[0].find_elements(By.XPATH,'.//div')[1].text)
 
                     std_dttm=datetime.now().isoformat(sep=' ',timespec='seconds')
-                    unix_dttm=datetime.now().timestamp()
 
-                    multiplier_series=[item['multiplier'] for item in self.series]
+                    multiplier_series=[item['multiplier'] for item in self.series][-sliding_window:]
                     occurences=sum(1 for num in multiplier_series if num > multiplier)
 
                     if len(multiplier_series)>0:
-                        probability=round((occurences/len(multiplier_series)),2)
-                        payout=round((bets*multiplier*probability),2)
-                        RTP=round((multiplier*probability),2)
-                    
+                        probability=occurences/len(multiplier_series)
+                        payout=bets*multiplier*probability
+                        RTP=multiplier*probability
 
-                    data={'id':round_id,'std_time':std_dttm,'multiplier':multiplier,'probability':probability,'bets':bets,'payout':payout,'rtp':RTP,'unix_time':unix_dttm}
+                        self.execute_trade(multiplier,self.target_multiplier)
+                        self.predict_multiplier(self.selection_multipliers,multiplier_series)
+
+
+
+                    data={'id':round_id,'std_time':std_dttm,'multiplier':multiplier,'probability':round(probability,6),'bets':bets,'payout':round(payout,2),'multiplier_rtp':round(RTP,2),'next_multiplier':self.target_multiplier,'next_multiplier_rtp':round(self.target_multiplier_rtp,2),'balance':round(self.balance,2)}
 
                     self.record=json.dumps(data,separators=(',',':'))
-
                     self.series.append(data)
 
                     with open(self.fileName,'w') as file:
@@ -130,11 +168,28 @@ class Navigator:
 
                     print(f'{w}{data}\n{w}')
                     round_id+=1
+                    self.selection_rtps=[]
+
                 time.sleep(0.1)
 
-        except (WebDriverException,StaleElementReferenceException,NoSuchWindowException,TimeoutException) as e:
-            print(f'{y} an error occured.Attempting to fix... {w}')
-            self.navigate_to_game()
+        except (AttributeError,TypeError,WebDriverException,StaleElementReferenceException,NoSuchWindowException,TimeoutException) as e:
+
+            if 'no such window' in str(e):
+                self.driver.quit()
+                logging.error(f'{y}game engine error - no such window exception\n{c}Attempting to fix...{w}')
+                self.__init__((self.phone,self.password),self.url)
+                self.start()
+
+            self.driver.quit()
+            logging.error(f'{y}game engine error - {e}\n{c}Attempting to fix...{w}')
+            self.__init__((self.phone,self.password),self.url)
+            self.start()        
+    
+    def start(self):
+        source_thread=threading.Thread(target=self.source)
+        self.navigate_to_game()
+        self.manage_data_backup('raw_data')
+        source_thread.start()
 
 mozzartGame=Navigator(('0113294793','Chri570ph3r.'),'https://www.mozzartbet.co.ke/en#/casino')
 mozzartGame.start()
