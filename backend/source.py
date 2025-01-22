9c603a0e7b5db691b3a8e5edbaf0e17bb4fb28af
krisskariuki <chri570ph3rk4riuki@gmail.com>
2025-01-22 19:48:55 +0300

    celeaned up unneccessary methods in the navigator class and left only three methods.(manage_data_backup,extract_data_from_game_engine,start). Also added a commandline argument for more control over the headless mode from the command prompt.Switched from print statements to logging for runtime messages except for the game results

--- backend/source.py
+++ backend/source.py
@@ -6,6 +6,7 @@ import logging
 import json
 import time
 import os
+import argparse
 
 w=Fore.WHITE
 g=Fore.GREEN
@@ -13,20 +14,20 @@ y=Fore.YELLOW
 c=Fore.CYAN
 m=Fore.MAGENTA
 
+logging.basicConfig(level=logging.INFO,format='%(asctime)s  %(levelname)s  %(message)s',datefmt='%Y-%m-%d %H:%M:%S')
+
+navigator_parser=argparse.ArgumentParser(description='for controlling the bots configuration parameters')
+navigator_parser.add_argument('--browse',action='store_false')
+navigator_arguments=navigator_parser.parse_args()
+
 class Navigator:
-    def __init__(self,credentials,targetUrl,headless=False):
-        self.url=targetUrl
+    def __init__(self,credentials,headless=False):
+        self.headless=headless
+        self.url='https://www.mozzartbet.co.ke/en#/casino'
         self.phone=credentials[0]
         self.password=credentials[1]
 
-        self.balance=0
-        self.stake=1
-
         self.fileName=None
-        self.target_multiplier=2.0
-        self.target_multiplier_rtp=0
-        self.selection_multipliers_rtps=[]
-        self.selection_multipliers=[1.2,1.5,1.75,2.0,2.25,2.5,3.0,3.5,4.0,5.0,6.0,7.0,8.0,9.0,10.0]
         self.record={}
         self.series=[]
 
@@ -34,7 +35,7 @@ class Navigator:
         service=Service(ChromeDriverManager().install())
 
         headModeArgs=['--ingore-certificate-errors','--disable-notifications']
-        headlessArgs=['--headless','--no-sandbox','--disable-gpu','--disable-dev-shm-usage','--enable-unsafe-swiftshader']
+        headlessArgs=['--headless','--no-sandbox','--disable-gpu','--disable-dev-shm-usage','--enable-unsafe-swiftshader']+headModeArgs
 
         args=headlessArgs if headless else headModeArgs
 
@@ -44,51 +45,27 @@ class Navigator:
         
         self.driver=webdriver.Chrome(options=options,service=service)
 
-        logging.basicConfig(level=logging.INFO,format='%(asctime)s | %(levelname)s | %(message)s',datefmt='%Y-%m-%d %H:%M:%S')
-
-    def manage_data_backup(self,fileName):
+    def manage_data_backup(self,fileName='raw_data'):
         file_id=1
-        if os.path.exists(f'raw_data/{fileName}.json'):
-            self.fileName=f'raw_data/{fileName}_{file_id}.json'
+        folderName='raw_data'
+
+        if os.path.exists(f'{folderName}/{fileName}.json'):
+            self.fileName=f'{folderName}/{fileName}_{file_id}.json'
             file_id+=1
 
         else:
-            self.fileName=f'raw_data/{fileName}.json'
+            self.fileName=f'{folderName}/{fileName}.json'
         
-    def predict_multiplier(self,multiplier_choices,multipliers_array):
-        rtp=0
-        rtp_series=[]
-
-        if len(multipliers_array) > 0:
-            for target_multiplier in multiplier_choices:
-                count=sum(1 for num in multipliers_array if num > target_multiplier)
-
-                probability=round((count/len(multipliers_array)),2)
-                rtp=target_multiplier*probability
-
-                rtp_series.append(round(rtp,2))
-                self.selection_multipliers_rtps.append([target_multiplier,round(rtp,4)])
-
-                selected_rtp_index=rtp_series.index(max(rtp_series))
-                self.target_multiplier=multiplier_choices[selected_rtp_index]
-                self.target_multiplier_rtp=rtp_series[selected_rtp_index]
-
-    def execute_trade(self,outcome_multiplier,predicted_multiplier):
-        if outcome_multiplier > predicted_multiplier:
-            self.balance+=(self.stake*predicted_multiplier)-self.stake
-        else:
-            self.balance-=self.stake
-
     def navigate_to_game(self):
 
         try:
             self.driver.get(self.url)
+            logging.info(self.driver.get_window_size())
             logging.info(f'{c}navigating to {self.url}...{w}')
 
             loginLink=WebDriverWait(self.driver,30).until(EC.element_to_be_clickable((By.XPATH,'//*[@class="login-link mozzart_ke"]')))
             logging.info(f'{c}logging in...{w}')
             loginLink.click()
-
 
             phoneInput=WebDriverWait(self.driver,30).until(EC.presence_of_element_located((By.XPATH,'//*[@placeholder="Mobile number"]')))
             logging.info(f'{c}writing phone input...{w}')
@@ -108,30 +85,24 @@ class Navigator:
             if 'no such window' in str(e):
                 self.driver.quit()
                 logging.error(f'{y}navigation procedure error - no such window exception\n{c}Attempting to fix...{w}')
-                self.__init__((self.phone,self.password),self.url)
+                self.__init__((self.phone,self.password),self.url,self.headless)
                 self.navigate_to_game()
 
             self.driver.quit()
             logging.error(f'{y}navigation procedure error- {e}\n{c}Attempting to fix...{w}')
-            self.__init__((self.phone,self.password),self.url)
+            self.__init__((self.phone,self.password),self.url,self.headless)
             self.navigate_to_game()
 
-
-    def source(self):
+    def extract_data_from_game_engine(self):
         previousMutipliers=None     
         round_id=1
-        sliding_window=22
-        std_dttm=None
+        dttm=None
         multiplier=0
         bets=0
-        probability=0
-        payout=0
-        RTP=0
-
                
         payoutsBlock=WebDriverWait(self.driver,30).until(EC.presence_of_element_located((By.XPATH,'//*[@class="payouts-block"]')))
         if payoutsBlock:
-            logging.info(f'{g}connected to game engine successfully{w}')
+            logging.info(f'{g}connected to game engine successfully\n\n{w}')
 
         try:
             while True:
@@ -142,23 +113,8 @@ class Navigator:
                     multiplier=float(latestMultipliers[0].text.replace('x',''))
                     bets=int(self.driver.find_element(By.XPATH,'//*[@class="all-bets-block d-flex justify-content-between align-items-center px-2 pb-1"]').find_elements(By.XPATH,'.//div')[0].find_elements(By.XPATH,'.//div')[1].text)
 
-                    std_dttm=datetime.now().isoformat(sep=' ',timespec='seconds')
-
-                    multiplier_series=[item['multiplier'] for item in self.series][-sliding_window:]
-                    occurences=sum(1 for num in multiplier_series if num > multiplier)
-
-                    if len(multiplier_series)>0:
-                        probability=occurences/len(multiplier_series)
-                        payout=bets*multiplier*probability
-                        RTP=multiplier*probability
-
-                        self.execute_trade(multiplier,self.target_multiplier)
-                        self.predict_multiplier(self.selection_multipliers,multiplier_series)
-
-
-
-                    data={'id':round_id,'std_time':std_dttm,'multiplier':multiplier,'probability':round(probability,6),'bets':bets,'payout':round(payout,2),'multiplier_rtp':round(RTP,2),'next_multiplier':self.target_multiplier,'next_multiplier_rtp':round(self.target_multiplier_rtp,2),'balance':round(self.balance,2)}
-
+                    dttm=datetime.now().isoformat(sep=' ',timespec='seconds')
+                    data={'id':round_id,'dttm':dttm,'multiplier':multiplier,'bets':bets}
                     self.record=json.dumps(data,separators=(',',':'))
                     self.series.append(data)
 
@@ -166,9 +122,8 @@ class Navigator:
                         fileSeries=json.dumps(self.series,indent=True,separators=(',',':'))
                         file.write(fileSeries)
 
-                    print(f'{w}{data}\n{w}')
+                    print(f'{c}{self.record}{w}')
                     round_id+=1
-                    self.selection_rtps=[]
 
                 time.sleep(0.1)
 
@@ -177,19 +132,19 @@ class Navigator:
             if 'no such window' in str(e):
                 self.driver.quit()
                 logging.error(f'{y}game engine error - no such window exception\n{c}Attempting to fix...{w}')
-                self.__init__((self.phone,self.password),self.url)
+                self.__init__((self.phone,self.password),self.url,self.headless)
                 self.start()
 
             self.driver.quit()
             logging.error(f'{y}game engine error - {e}\n{c}Attempting to fix...{w}')
-            self.__init__((self.phone,self.password),self.url)
+            self.__init__((self.phone,self.password),self.url,self.headless)
             self.start()        
     
     def start(self):
-        source_thread=threading.Thread(target=self.source)
+        source_thread=threading.Thread(target=self.extract_data_from_game_engine)
         self.navigate_to_game()
-        self.manage_data_backup('raw_data')
+        self.manage_data_backup(fileName='raw_data')
         source_thread.start()
 
-mozzartGame=Navigator(('0113294793','Chri570ph3r.'),'https://www.mozzartbet.co.ke/en#/casino')
+mozzartGame=Navigator(('0113294793','Chri570ph3r.'),navigator_arguments.browse)
 mozzartGame.start()
