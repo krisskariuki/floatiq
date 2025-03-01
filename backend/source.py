9bff3c6a25e032e7962c42beaa79310988032e56
krisskariuki <chri570ph3rk4riuki@gmail.com>
2025-03-01 17:02:02 +0300

    -added Flask to expose the collected data to an api -changed camel-case variables to snake_case for better readability

--- backend/source.py
+++ backend/source.py
@@ -3,9 +3,13 @@ from datetime import datetime
 from colorama import Fore
 from time import sleep
 from dotenv import load_dotenv
+from flask import Flask,Response,jsonify
+from flask_cors import CORS
+from pathlib import Path
 import threading
 import logging
 import json
+import random
 import os
 import argparse
 import pandas as pd
@@ -25,6 +29,9 @@ navigator_parser=argparse.ArgumentParser(description='for controlling the bots c
 navigator_parser.add_argument('--browse',action='store_false')
 navigator_arguments=navigator_parser.parse_args()
 
+app=Flask(__name__)
+CORS(app)
+
 class Navigator:
     def __init__(self,credentials,headless=False):
         self.headless=headless
@@ -32,17 +39,20 @@ class Navigator:
         self.phone=credentials[0]
         self.password=credentials[1]
 
-        self.fileName=None
-        self.record={}
+        self.file_name=None
+        self.record=None
         self.series=[]
+
+        self.record_lock=threading.Lock()
+        self.series_lock=threading.Lock()
 
         options=webdriver.ChromeOptions()
         service=Service(ChromeDriverManager().install())
 
-        headModeArgs=['--ingore-certificate-errors','--disable-notifications']
-        headlessArgs=['--headless','--no-sandbox','--disable-gpu','--disable-dev-shm-usage','--enable-unsafe-swiftshader']+headModeArgs
+        headmode_args=['--ingore-certificate-errors','--disable-notifications']
+        healess_args=['--headless','--no-sandbox','--disable-gpu','--disable-dev-shm-usage','--enable-unsafe-swiftshader']+headmode_args
 
-        args=headlessArgs if headless else headModeArgs
+        args=healess_args if headless else headmode_args
 
         for arg in args:
             options.add_argument(arg)
@@ -50,39 +60,39 @@ class Navigator:
         
         self.driver=webdriver.Chrome(options=options,service=service)
 
-    def manage_data_backup(self,folderName='db',fileName='raw'):
+    def manage_data_backup(self,folder_name='db',file_name='raw'):
         file_id=0
 
-        while (os.path.exists(f'{folderName}/{fileName}_{file_id}.csv')):
+        while (os.path.exists(f'{folder_name}/{file_name}_{file_id}.csv')):
             file_id+=1
 
-        file_to_check=f'{folderName}/{fileName}_{file_id}.csv'
+        file_to_check=f'{folder_name}/{file_name}_{file_id}.csv'
 
         directory=os.path.dirname(file_to_check)
         os.makedirs(directory,exist_ok=True)
 
-        self.fileName=file_to_check
+        self.file_name=file_to_check
 
     def navigate_to_game(self):
         try:
             self.driver.get(self.url)
             logging.info(f'{c}navigating to {self.url}...{w}')
 
-            loginLink=WebDriverWait(self.driver,30).until(EC.element_to_be_clickable((By.XPATH,'//*[@class="login-link mozzart_ke"]')))
+            login_link=WebDriverWait(self.driver,30).until(EC.element_to_be_clickable((By.XPATH,'//*[@class="login-link mozzart_ke"]')))
             logging.info(f'{c}logging in...{w}')
-            loginLink.click()
+            login_link.click()
 
-            phoneInput=WebDriverWait(self.driver,30).until(EC.presence_of_element_located((By.XPATH,'//*[@placeholder="Mobile number"]')))
+            phone_input=WebDriverWait(self.driver,30).until(EC.presence_of_element_located((By.XPATH,'//*[@placeholder="Mobile number"]')))
             logging.info(f'{c}writing phone input...{w}')
-            phoneInput.send_keys(self.phone)
+            phone_input.send_keys(self.phone)
 
-            passwordInput=WebDriverWait(self.driver,30).until(EC.presence_of_element_located((By.XPATH,'//*[@placeholder="Password"]')))
+            password_input=WebDriverWait(self.driver,30).until(EC.presence_of_element_located((By.XPATH,'//*[@placeholder="Password"]')))
             logging.info(f'{c}writing password input...{w}')
-            passwordInput.send_keys(self.password + Keys.RETURN)
+            password_input.send_keys(self.password + Keys.RETURN)
 
-            gameButton=WebDriverWait(self.driver,30).until(EC.element_to_be_clickable((By.XPATH,'//*[@alt="Aviator"]')))
+            game_button=WebDriverWait(self.driver,30).until(EC.element_to_be_clickable((By.XPATH,'//*[@alt="Aviator"]')))
             sleep(1)                                         
-            gameButton.click()
+            game_button.click()
             logging.info(f'{c}navigating to game engine...{w}')
 
         except (AttributeError,WebDriverException,StaleElementReferenceException,NoSuchWindowException,TimeoutException) as e:
@@ -100,35 +110,35 @@ class Navigator:
 
     def extract_data_from_game_engine(self):
         old=None
+        round_id=0
+
         def check_for_new_data(recent):
-            nonlocal old
-            timestamp=None
+            nonlocal old,round_id
             multiplier=0
-            bets=0
 
             if old!=recent:
                     old=recent
+                    round_id+=1
                     multiplier=float(recent[0].text.replace('x','').replace(',',''))
-                    bets=int(self.driver.find_element(By.XPATH,'//*[@class="all-bets-block d-flex justify-content-between align-items-center px-2 pb-1"]').find_elements(By.XPATH,'.//div')[0].find_elements(By.XPATH,'.//div')[1].text)
+                    std_time=datetime.now().isoformat(sep=' ',timespec='seconds')
+                    unix_time=int(datetime.now().timestamp())
 
-                    timestamp=datetime.now().isoformat(sep=' ',timespec='seconds')
-                    data={'multiplier':multiplier,'bets':bets,'timestamp':timestamp}
+                    data={'round_id':round_id,'multiplier':multiplier,'std_time':std_time,'unix_time':unix_time}
                     self.record=json.dumps(data,separators=(',',':'))
                     self.series.append(data)
 
-                    df=pd.DataFrame(self.series)
-                    df.to_csv(self.fileName,index=False)
+                    pd.DataFrame([data]).to_csv(self.file_name, mode='a', index=False, header=not Path(self.file_name).exists())
 
                     print(f'{b}{self.record}{w}\n')
             
         try:
-            payoutsBlock=WebDriverWait(self.driver,30).until(EC.presence_of_element_located((By.XPATH,'//*[@class="payouts-block"]')))
-            if payoutsBlock:
+            payouts_block=WebDriverWait(self.driver,30).until(EC.presence_of_element_located((By.XPATH,'//*[@class="payouts-block"]')))
+            if payouts_block:
                 logging.info(f'{g}connected to game engine successfully\n\n{w}')
 
             while True:
-                latestMultipliers=self.driver.find_element(By.CLASS_NAME,'payouts-block').find_elements(By.CLASS_NAME,'bubble-multiplier')
-                check_for_new_data(latestMultipliers)
+                latest_multipliers=self.driver.find_element(By.CLASS_NAME,'payouts-block').find_elements(By.CLASS_NAME,'bubble-multiplier')
+                check_for_new_data(latest_multipliers)
                 sleep(0.1)
 
         except (AttributeError,TypeError,WebDriverException,StaleElementReferenceException,NoSuchWindowException,TimeoutException) as e:
@@ -144,11 +154,55 @@ class Navigator:
             self.__init__((self.phone,self.password),self.headless)
             self.start()        
     
+    def yield_data(self):
+        local_record=None
+        
+        while True:
+            with self.record_lock:
+                current_record=self.record
+            
+            if local_record!=current_record:
+                local_record=current_record
+                yield f'data: {self.record} \n\n'
+            
+            else:
+                multiplier=round(random.uniform(-0.50,0.50),2)
+                std_time=datetime.now().isoformat(sep=' ',timespec='seconds')
+                unix_time=int(datetime.now().timestamp())
+                temp_record={'multiplier':multiplier,'std_time':std_time,'unix_time':unix_time}
+
+                yield f"data: {json.dumps(temp_record)}\n\n"
+            
+            sleep(1)
+
+    def expose(self):
+        @app.route('/raw/stream')
+        def expose_data():
+            return Response(self.yield_data(),content_type='text/event-stream')
+
+        @app.route('/raw/latest')
+        def latest():
+            return self.record
+
+        @app.route('/raw/series')
+        def history():
+            return jsonify(self.series)
+        
+        app.run(threaded=True,host='0.0.0.0',port=8080)
+    
     def start(self):
-        self.manage_data_backup()
         source_thread=threading.Thread(target=self.extract_data_from_game_engine)
+        yielder_thread=threading.Thread(target=self.yield_data,daemon=True)
+
+        self.manage_data_backup()
         self.navigate_to_game()
+
         source_thread.start()
+        yielder_thread.start()
+        self.expose()
+
+
+       
 
 mozzartGame=Navigator((PHONE,PASSWORD),navigator_arguments.browse)
 mozzartGame.start()
