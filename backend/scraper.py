d95299f718a685f98a2d3b39b282b24feb245f3e
krisskariuki <chri570ph3rk4riuki@gmail.com>
2025-03-03 16:23:59 +0300

    utility script for web scraping.

--- backend/scraper.py
+++ backend/scraper.py
@@ -12,109 +12,90 @@ y=Fore.YELLOW
 c=Fore.CYAN
 m=Fore.MAGENTA
 
-class Action:
-    def __init__(self,action:str='locate',attribute:str='',delay:int=5,inputValue:str=None,callback=None)->None:
-        self.action=action
-        self.attribute=attribute
-        self.delay=delay
-        self.inputValue=inputValue
-        self.callback=callback
-
-class Navigator:
-    def __init__(self,webUrl:str,headless:bool=False)->None:
-        self.webUrl=webUrl
-        self.data=None
+class Scraper:
+    def __init__(self,target_url:str,headless:bool=False)->None:
+        self.target_url=target_url
+        self.headless=headless
+        self.wait_time=30
+        self.record=None
+        self.series=None
+        self.actions_array=[]
+        
         options=webdriver.ChromeOptions()
         service=Service(ChromeDriverManager().install())
         
-        headmodeArgs=['--ignore-certificate-errors','--disable-notifications']
-        headlessArgs=['--headless','--no-sandbox','--disable-gpu','--disable-dev-shm-usage',' --enable-unsafe-swiftshader']+headmodeArgs
+        headmode_args=['--ignore-certificate-errors','--disable-notifications']
+        headless_args=['--headless','--no-sandbox','--disable-gpu','--disable-dev-shm-usage','--enable-unsafe-swiftshader']+headmode_args
         
-        args=headlessArgs if headless else headmodeArgs
+        args=headless_args if headless else headmode_args
         
         for arg in args:
             options.add_argument(arg)
         options.add_experimental_option('detach',True)
 
         self.driver=webdriver.Chrome(service=service,options=options)
-                
-    def action(self,action):                
+    
+    def action(self,action='locate',attribute='',sleep_time=0,message='',input_value=''):
+        action={key:value for key,value in locals().items() if key!="self"}
+        self.actions_array.append(action)
+    
+    def parse(self,action):
         def locate():
-            state=False
-            element=WebDriverWait(self.driver,60).until(EC.presence_of_element_located((By.XPATH,f'//*[@{action.attribute}]')))
-
+            element=WebDriverWait(self.driver,self.wait_time).until(EC.presence_of_element_located((By.XPATH,f'//*[@{action["attribute"]}]')))
             if element:
-                state=True
-                print(f'{g}{self.driver.title}{w}')
-
-            else :
-                print('unable to locate element')
-
-            return state
-        
-        def click()->None:
-            state=False
-            element=WebDriverWait(self.driver,60).until(EC.element_to_be_clickable((By.XPATH,f'//*[@{action.attribute}]')))
-
-            if element:
-                state=True
-                element.click()
-
-            return state
-        
-        def write()->None:
-            state=False
-            element=WebDriverWait(self.driver,60).until(EC.presence_of_element_located((By.XPATH,f'//*[@{action.attribute}]')))
-
-            if element:
-                state=True
-                element.send_keys(action.inputValue)
-
-            return state
-        
-        def send()->None:
-            state=False
-            element=WebDriverWait(self.driver,60).until(EC.presence_of_element_located((By.XPATH,f'//*[@{action.attribute}]')))
+                print(f'{g}element located{w}')
+                return True
+            else:
+                print(f'{r}element not located{w}')
+                return False
             
-            if element:
-                state=True
-                element.send_keys(action.inputValue+Keys.RETURN)
+        def click():
+            element=WebDriverWait(self.driver,self.wait_time).until(EC.element_to_be_clickable((By.XPATH,f'//*[@{action["attribute"]}]')))
+            element.click()
+        
+        def write():
+            element=WebDriverWait(self.driver,self.wait_time).until(EC.presence_of_element_located((By.XPATH,f'//*[@{action["attribute"]}]')))
+            element.send_keys(action['input_value'])
+        
+        def send():
+            element=WebDriverWait(self.driver,self.wait_time).until(EC.presence_of_element_located((By.XPATH,f'//*[@{action["attribute"]}]')))
+            element.send_keys(action['input_value']+Keys.RETURN)
+        
+        def execute(function):
+            time.sleep(action['sleep_time'])
+            if action['message']:
+                print(f'{c}{action["message"]}{w}')
+                
+            function()
 
-            return state
-
-        def custom()->None:
-            try:
-                state=False
-                element=WebDriverWait(self.driver,60).until(EC.presence_of_element_located((By.XPATH,f'//*[@{action.attribute}]')))
-
-                if element:
-                    state=True
-                return state
-            
-            finally:
-                threading.Thread(target=action.callback,args=({'driver':self.driver,'locator':By},)).start()
-
-        def apply_delay(function):
-            # while action.delay > 0:
-            #     if function():
-            #         break
-            #     action.delay-=1
-                time.sleep(action.delay)
-                function()
-
-        actionHashMap={
+        action_table={
             'locate':locate,
             'click':click,
             'write':write,
-            'send':send,
-            'custom':custom
+            'send':send
             }
         
-        return apply_delay(actionHashMap[action.action])
-
+        execute(action_table[action['action']])
     
-    def navigate(self,actions):
-        self.driver.get(self.webUrl)
+    def navigate(self):
+        while True:
+            try:
+                self.driver.get(self.target_url)
+                print(f'{c}navigating to {self.target_url}...{w}')
+                for action in self.actions_array:
+                    try:
+                        self.parse(action)
 
-        for action in actions:
-            self.action(action)
+                    except Exception as e:
+                        print(f'{r}an error occured during navigation\n{w}')
+                        raise
+                    
+                print(f'{g}all actions completed successfully{w}')
+
+                break
+
+            except Exception as e:
+                print(f'{m}{e}\n{c}restarting...{w}')
+                self.driver.quit()
+                time.sleep(1)
+                self.__init__(self.target_url,self.headless)
