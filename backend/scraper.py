02a1e036ba3e2a19ce372cb05a6b9fe481a3cbd4
krisskariuki <chri570ph3rk4riuki@gmail.com>
2025-03-04 11:20:03 +0300

    -fixed self.series by initializing it as an empty list instead of None -added a watch() method to the Scraper class that will allow for the user to watch the scraping of data in real time

--- backend/scraper.py
+++ backend/scraper.py
@@ -1,6 +1,8 @@
 from selenium_imports import *
 from datetime import datetime
 from colorama import Fore
+from pathlib import Path
+import pandas as pd
 import time
 import sys
 import os
@@ -22,7 +24,7 @@ class Scraper:
 
         self.file_name=None
         self.record=None
-        self.series=None
+        self.series=[]
         
         self.actions_array=[]
         self.chromedriver_path=ChromeDriverManager().install()
@@ -68,6 +70,40 @@ class Scraper:
         action={key:value for key,value in locals().items() if key!="self"}
         self.actions_array.append(action)
     
+    def watch(self):
+        old,round_id,multiplier=None,0,0
+
+        def check_for_new_data(recent):
+            nonlocal old,round_id,multiplier
+
+            if old!=recent:
+                    old=recent
+                    round_id+=1
+                    multiplier=float(recent[0].text.replace('x','').replace(',',''))
+                    std_time=datetime.now().isoformat(sep=' ',timespec='seconds')
+                    unix_time=int(datetime.now().timestamp())
+
+                    data={'round_id':round_id,'multiplier':multiplier,'std_time':std_time,'unix_time':unix_time}
+                    self.record=data
+                    self.series.append(data)
+                    
+                    # pd.DataFrame([data]).to_csv(self.file_name, mode='a', index=False, header=not Path(self.file_name).exists())
+
+                    print(f'{b}{self.record}{w}\n')
+        try:
+            payouts_block=WebDriverWait(self.driver,self.wait_time).until(EC.presence_of_element_located((By.XPATH,'//*[@class="payouts-block"]')))
+            if payouts_block:
+                print(f'{g}connected to game engine successfully\n\n{w}')
+
+            while True:
+                latest_multipliers=self.driver.find_element(By.CLASS_NAME,'payouts-block').find_elements(By.CLASS_NAME,'bubble-multiplier')
+                check_for_new_data(latest_multipliers)
+                time.sleep(0.1)
+        except Exception as e:
+            print(f'{r}game engine error!\n{c}restarting...{w}')
+            self.restart_driver()
+                
+
     def parse(self,action):
         def locate():
             element=WebDriverWait(self.driver,self.wait_time).until(EC.presence_of_element_located((By.XPATH,f'//*[@{action["attribute"]}]')))
@@ -89,6 +125,7 @@ class Scraper:
         def send():
             element=WebDriverWait(self.driver,self.wait_time).until(EC.presence_of_element_located((By.XPATH,f'//*[@{action["attribute"]}]')))
             element.send_keys(action['input_value']+Keys.RETURN)
+
         
         def execute(function):
             time.sleep(action['sleep_time'])
@@ -106,7 +143,7 @@ class Scraper:
         
         execute(action_table[action['action']])
     
-    def navigate(self,retries=5):
+    def navigate(self,callback='',retries=5):
         while retries>0:
             try:
                 self.driver.get(self.target_url)
@@ -116,11 +153,14 @@ class Scraper:
                         self.parse(action)
 
                     except Exception as e:
-                        print(f'{y}navigation procedure error...\n{w}')
+                        print(f'{y}navigation procedure error!\n{w}')
                         raise
                     
-                print(f'{g}navigation procedure successful{w}')
-                return
+                if callable(callback):
+                    callback()
+                else:
+                    print(f'{g}navigation procedure successful{w}')
+                    return
 
             except Exception as e:
                 print(f'{b}[{retries}]\n{c}restarting...{w}')
