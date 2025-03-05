44d2e926bb40da41703276a208d17fa6e5101a46
krisskariuki <chri570ph3rk4riuki@gmail.com>
2025-03-05 03:28:21 +0300

    done major variable renaming throughout the entire file

--- backend/transformer.py
+++ backend/transformer.py
@@ -11,100 +11,99 @@ app=Flask(__name__)
 CORS(app)
 
 class Transformer:
-    def __init__(self,sourceUrl,token,timeframe):
-        self.source=SSEClient(sourceUrl)
+    def __init__(self,source_url,token,time_frame):
+        self.sse_source=SSEClient(source_url)
         self.flop=float(token['flop'])
-        self.time_unit=timeframe["unit"]
-        self.time_step=timeframe["interval"]
+        self.time_unit=time_frame["unit"]
+        self.time_step=time_frame["interval"]
         
-        self.id=0
-        self.multiplier=0
-        self.Open=0
-        self.High=float('-inf')
-        self.Low=float('inf')
-        self.Close=0
-        self.Coef=0
-        self.traffic=0
-        self.Dttm=datetime.now().timestamp()
         self.record={}
         self.series=[]
         
-        threading.Thread(target=self.TRANSFORM,daemon=True).start()
-        self.SERVE_SERIES()
-        self.SERVE_LATEST()
-        self.SERVE_STREAM()
-        
-    def TIMER(self):
-        timerRef={
+    def start(self):
+        threading.Thread(target=self.transform,daemon=True).start()
+
+    def timer(self):
+        timer_table={
             'second':lambda T:datetime.now().second%T==0,
             'minute':lambda T:datetime.now().minute%T==0 and datetime.now().second==0,
             'hour':lambda T:datetime.now().hour%T==0 and datetime.now().minute==0 and datetime.now().second==0,
             'day':lambda T:datetime.now().day%T==0 and datetime.now().hour==0 and datetime.now().minute==0 and datetime.now().second==0,
         }
-        return timerRef[self.time_unit](self.time_step)
+        return timer_table[self.time_unit](self.time_step)
             
-    def GENERATOR(self):
+    def generator(self):
         while True:
             yield f"data:{json.dumps(self.record)}\n\n"
             time.sleep(1)
             
-    def TRANSFORM(self):
-        for record in self.source:
-            record=json.loads(record.data)
+    def transform(self):
+        multiplier=0
+        Open=0
+        High=float('-inf')
+        Low=float('inf')
+        Close=0
+        Coeff=0
+        std_time=datetime.now().strftime('%Y-%m-%d %H:M%:%S')
+        unix_time=datetime.now().timestamp()
+        
+        for item in self.sse_source:
+            record=json.loads(item.data)
             
-            self.multiplier=record['multiplier']
+            multiplier=record['multiplier']
+            std_time=record['std_time']
 
-            if self.multiplier>=1:
-                if self.multiplier>self.flop:
-                    self.Coef+=self.flop-1
-                    self.Close+=self.flop-1
+            if multiplier>=1:
+                if multiplier>self.flop:
+                    Coeff+=self.flop-1
+                    Close+=self.flop-1
                 else:
-                    self.Coef-=1
-                    self.Close-=1
+                    Coeff-=1
+                    Close-=1
             else:
-                self.Close=self.Coef+round(random.uniform(-0.5,0.5),2)
+                Close=Coeff+round(random.uniform(-0.5,0.5),2)
                 
-            self.High=max(self.Open,self.High,self.Close)
-            self.Low=min(self.Open,self.Low,self.Close)
-            self.record={'time':self.Dttm,'multiplier':self.multiplier,'coef':self.Coef,'open':round(self.Open,2),'high':round(self.High,2),'low':round(self.Low,2),'close':round(self.Close,2)}
+            High=max(Open,High,Close)
+            Low=min(Open,Low,Close)
+            self.record={'std_time':std_time,'unix_time':unix_time,'multiplier':multiplier,'coef':Coeff,'open':round(Open,2),'high':round(High,2),'low':round(Low,2),'close':round(Close,2)}
             
-            if self.TIMER():
-                self.Dttm=datetime.now().timestamp()
-                self.Open=self.Close
-                self.High=float('-inf')
-                self.Low=float('inf')
+            if self.timer():
+                unix_time=datetime.now().timestamp()
+                Open=Close
+                High=float('-inf')
+                Low=float('inf')
                 
                 self.series.append(self.record)
                 
             time.sleep(1)
+    def expose(self):
+        def serve_series():
+            EPoint=f"series/{time_frame['name']}/{token['name']}"
+            url=f"/market/{EPoint}"
+
+            app.add_url_rule(url,endpoint=EPoint,view_func=lambda:jsonify(self.series))
             
-    def SERVE_SERIES(self):
-        EPoint=f"series/{timeframe['name']}/{token['name']}"
-        url=f"/market/{EPoint}"
+        def serve_latest():
+            EPoint=f"latest/{time_frame['name']}/{token['name']}"
+            url=f"/market/{EPoint}"
 
-        app.add_url_rule(url,endpoint=EPoint,view_func=lambda:jsonify(self.series))
+            app.add_url_rule(url,endpoint=EPoint,view_func=lambda:jsonify(self.record))
         
-    def SERVE_LATEST(self):
-        EPoint=f"latest/{timeframe['name']}/{token['name']}"
-        url=f"/market/{EPoint}"
+        def serve_stream():
+            EPoint=f"stream/{time_frame['name']}/{token['name']}"
+            url=f"/market/{EPoint}"
+            app.add_url_rule(url,endpoint=EPoint,view_func=lambda:Response(self.generator(),mimetype='text/event-stream'))
 
-        app.add_url_rule(url,endpoint=EPoint,view_func=lambda:jsonify(self.record))
-    
-    def SERVE_STREAM(self):
-        EPoint=f"stream/{timeframe['name']}/{token['name']}"
-        url=f"/market/{EPoint}"
-
-        app.add_url_rule(url,endpoint=EPoint,view_func=lambda:Response(self.GENERATOR(),mimetype='text/event-stream'))
+        app.run(debug=True,host='0.0.0.0',port=8000)
 
 with open('config/format.json','r') as file:
     format_data=json.load(file)
 
-timeframes=format_data['timeframes']
+time_frames=format_data['time_frames']
 tokens=format_data['tokens']
 
-for timeframe in timeframes:
+for time_frame in time_frames:
     for token in tokens:
-        Transformer('http://127.0.0.1:8080/raw/stream',token,timeframe)
-        
-if __name__=='__main__':
-    app.run(debug=True,host='0.0.0.0',port=8000)
+        transformer=Transformer('http://127.0.0.1:8080/raw/stream',token,time_frame)
+        transformer.start()
+        transformer.expose()
