1195c77d4fc94f68f6ae6b7ff9c8d00cbb3bed76
krisskariuki <chri570ph3rk4riuki@gmail.com>
2025-03-07 14:09:21 +0300

    -added a new action "callback" -added a new expose_data method for serving collected data to an API -removed the *args argument from the navigate method since there is a new callback action for this purpose

--- backend/scraper.py
+++ backend/scraper.py
@@ -3,7 +3,8 @@ from datetime import datetime
 from colorama import Fore,init
 from pathlib import Path
 import pandas as pd
-from types import FunctionType
+from flask import Flask,Response,jsonify
+from flask_cors import CORS
 import time
 import sys
 import os
@@ -19,6 +20,9 @@ c=Fore.CYAN
 m=Fore.MAGENTA
 b=Fore.LIGHTBLACK_EX
 
+
+app=Flask(__name__)
+CORS(app)
 # consider implementing an action for callbacks so that custom & internal functions can be pushed to the actions_array. This might solve many blockades & complexities
 class Scraper:
     def __init__(self,target_url:str,headless:bool=False,wait_time:int=10,retries:int=5)->None:
@@ -81,10 +85,38 @@ class Scraper:
             if self.record and isinstance(self.record,dict):
                 pd.DataFrame([self.record]).to_csv(self.file_name,mode='a',index=False,header=False)
 
-    def action(self,action='locate',attribute='',sleep_time=0,message='',input_value=''):
+    def action(self,action='locate',attribute='',sleep_time=0,message='',input_value='',callback=None,args=[]):
         action={key:value for key,value in locals().items() if key!="self"}
         self.actions_array.append(action)
     
+    def expose_data(self):      
+        @app.route('/mozzart_aviator/latest',methods=['GET'])
+        def get_latest():
+            with self.record_lock:
+                return jsonify(self.record)
+
+        @app.route('/mozzart_aviator/history',methods=['GET'])
+        def get_history():
+            with self.series_lock:
+                return jsonify(self.series)
+
+        @app.route('/mozzart_aviator/stream',methods=['GET'])
+        def stream_data():
+            def event_stream():
+                last_seen=None
+                while True:
+                    with self.record_lock:
+                        if self.record and self.record!=last_seen:
+                            last_seen=self.record.copy()
+                            yield f"data:{json.dumps(last_seen,separators=(',',':'))}\n\n"
+                    time.sleep(1)
+            return Response(event_stream(),mimetype='text/event-stream')
+
+        def run_app():
+            app.run(host='0.0.0.0',port=8080,threaded=True)
+        
+        threading.Thread(target=run_app,daemon=True).start()
+        
     def watch_aviator(self):
         old=None
 
@@ -107,20 +139,25 @@ class Scraper:
                         self.save_record()
 
                     print(f'{b}{json.dumps(self.record,separators=(",",":"))}\n')
-        try:
-            payouts_block=WebDriverWait(self.driver,self.wait_time).until(EC.presence_of_element_located((By.XPATH,'//*[@class="payouts-block"]')))
-            if payouts_block:
-                print(f'{g}connected to game engine successfully')
-                self.manage_backup()
+        def run_aviator():
+            try:
+                payouts_block=WebDriverWait(self.driver,self.wait_time).until(EC.presence_of_element_located((By.XPATH,'//*[@class="payouts-block"]')))
+                if payouts_block:
+                    print(f'{g}connected to game engine successfully')
+                    self.manage_backup()
 
-            while True:
-                latest_multipliers=self.driver.find_element(By.CLASS_NAME,'payouts-block').find_elements(By.CLASS_NAME,'bubble-multiplier')
-                check_for_new_data(latest_multipliers)
-                time.sleep(1)
-        except Exception as e:
-            print(f'{r}game engine error!\n{y}{e}\n{c}restarting...')
-            self.restart_driver()
-                
+                while True:
+                    try:
+                        latest_multipliers=self.driver.find_element(By.CLASS_NAME,'payouts-block').find_elements(By.CLASS_NAME,'bubble-multiplier')
+                        check_for_new_data(latest_multipliers)
+                        time.sleep(1)
+                    except:
+                        raise
+            except Exception as e:
+                print(f'{r}game engine error!\n{y}{e}\n{c}restarting...')
+                self.navigate()
+            
+        threading.Thread(target=run_aviator).start()
 
     def parse_action(self,action):
         def locate():
@@ -144,7 +181,12 @@ class Scraper:
             element=WebDriverWait(self.driver,self.wait_time).until(EC.presence_of_element_located((By.XPATH,f'//*[@{action["attribute"]}]')))
             element.send_keys(action['input_value']+Keys.RETURN)
 
-        
+        def callback():
+            if callable(action['callback']):
+                action['callback'](*(action['args']))
+            else:
+                raise TypeError("callback is not of type 'function'")
+
         def execute(function):
             time.sleep(action['sleep_time'])
             if action['message']:
@@ -156,12 +198,13 @@ class Scraper:
             'locate':locate,
             'click':click,
             'write':write,
-            'send':send
+            'send':send,
+            'callback':callback
             }
         
         execute(action_table[action['action']])
     
-    def navigate(self,*args:FunctionType):
+    def navigate(self):
         while self.retries>0:
             try:
                 self.driver.get(self.target_url)
@@ -173,12 +216,9 @@ class Scraper:
                     except Exception as e:
                         print(f'{y}navigation procedure error!\n{y}{e}\n')
                         raise
-                for arg in args:   
-                    if callable(arg):
-                        arg()
-                    else:
-                        print(f'{g}navigation procedure successful')
-                        return
+
+                print(f'{g}navigation procedure successful')
+                return
 
             except:
                 print(f'{b}[{self.retries}]\n{c}restarting...')
