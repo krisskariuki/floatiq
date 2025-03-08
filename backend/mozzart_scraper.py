45b68d5740acff49eefb4f31ad36951f9ac85529
krisskariuki <chri570ph3rk4riuki@gmail.com>
2025-03-08 09:39:13 +0300

    -moved the scraper.expose_data method from the action callback to prevent rewriting endpoints     whenever an error occurs

--- backend/mozzart_scraper.py
+++ backend/mozzart_scraper.py
@@ -20,10 +20,9 @@ scraper.action(action='click',attribute='class="login-link mozzart_ke"',message=
 scraper.action(action='write',attribute='placeholder="Mobile number"',message='writing phone input...',input_value=PHONE)
 scraper.action(action='send',attribute='placeholder="Password"',message='writing password input...',input_value=PASSWORD)
 scraper.action(action='click',attribute='alt="Aviator"',message='connecting to game engine...',sleep_time=1)
-
 scraper.action(action='callback',callback=scraper.watch_aviator)
-scraper.action(action='callback',callback=scraper.expose_data)
 
+scraper.expose_data()
 scraper.navigate()
 
 main_thread()
