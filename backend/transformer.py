438083ccaca55576debbbc5a354e95f2ca00abc4
krisskariuki <chri570ph3rk4riuki@gmail.com>
2025-03-08 22:48:09 +0300

    major modifications to the entire script

--- backend/transformer.py
+++ backend/transformer.py
@@ -1,109 +1,119 @@
-from flask import Flask,jsonify,Response
-from flask_cors import CORS
-from datetime import datetime
+from scraper import main_thread,w,c,r,g,b,y
 from sseclient import SSEClient
-import time
+from datetime import datetime
+from colorama import init
 import json
+import time
 import threading
-import random
+import sys
+import socket
 
-app=Flask(__name__)
-CORS(app)
+ip_address=socket.gethostbyname(socket.gethostname())
+
+def start_program_at_0sec():
+    while datetime.now().second!=0:
+        remaining=60-datetime.now().second
+        print(f"Starting in: {c}{remaining}{w} seconds",end="\r")
+        time.sleep(0.5)
+
+init(autoreset=True)
 
 class Transformer:
-    def __init__(self,source_url,token,time_frame):
-        self.sse_source=SSEClient(source_url)
-        self.flop=float(token['flop'])
-        self.time_unit=time_frame["unit"]
-        self.time_step=time_frame["interval"]
-        
-        self.record={}
-        self.series=[]
-        
-    def start(self):
-        threading.Thread(target=self.transform,daemon=True).start()
+    def __init__(self,source_url):
+        self.source_url=source_url
+        self.sse_source=''
+        self.dataset={}
+    def connect(self):
+        try:
+            self.sse_source=SSEClient(self.source_url)
+            print(f'\n\n{g}connected to: {c}{self.source_url}\n')
+        except :
+            print(f'\n\n{r}connection error!\n{y}failed to connect to: {c}{self.source_url}\n')
+            sys.exit(1)
 
-    def timer(self):
-        timer_table={
-            'second':lambda T:datetime.now().second%T==0,
-            'minute':lambda T:datetime.now().minute%T==0 and datetime.now().second==0,
-            'hour':lambda T:datetime.now().hour%T==0 and datetime.now().minute==0 and datetime.now().second==0,
-            'day':lambda T:datetime.now().day%T==0 and datetime.now().hour==0 and datetime.now().minute==0 and datetime.now().second==0,
-        }
-        return timer_table[self.time_unit](self.time_step)
-            
-    def generator(self):
-        while True:
-            yield f"data:{json.dumps(self.record)}\n\n"
-            time.sleep(1)
-            
-    def transform(self):
-        multiplier=0
-        Open=0
-        High=float('-inf')
-        Low=float('inf')
-        Close=0
-        Coeff=0
-        std_time=datetime.now().strftime('%Y-%m-%d %H:M%:%S')
-        unix_time=datetime.now().timestamp()
-        
-        for item in self.sse_source:
-            record=json.loads(item.data)
-            
-            multiplier=record['multiplier']
-            std_time=record['std_time']
+    
+    def transform(self,resource_file):
+        with open(resource_file,'r') as file:
+            resource_table=json.load(file)
 
-            if multiplier>=1:
-                if multiplier>self.flop:
-                    Coeff+=self.flop-1
-                    Close+=self.flop-1
+        time_frames=resource_table['time_frames']
+        tokens=resource_table['tokens']
+
+        for time_frame in time_frames:
+            for token in tokens:
+                self.generate_metrics(token['flop'],time_frame['name'])
+
+    def quantifier(self,target_multiplier,time_frame):
+        last_reset_time=time.time()
+
+        def timer(time_frame):
+            nonlocal last_reset_time
+            time_unit,time_step=time_frame.split('_')
+            time_step=int(time_step)
+            
+            current_time=time.time()
+            elapsed_time=current_time-last_reset_time
+
+            time_map={
+                "second":time_step,
+                "minute":time_step*60,
+                "hour":time_step*3600,
+                "day":time_step*86400,
+            }
+
+            if elapsed_time>=time_map.get(time_unit):
+                last_reset_time=current_time
+                return True
+            return False
+        
+        def generate_metrics():
+            Open=0
+            High=float('-inf')
+            Low=float('inf')
+            Close=0
+            std_time=datetime.now().isoformat(sep=' ',timespec='seconds')
+            unix_time=int(datetime.now().timestamp())
+            interval_time=std_time
+
+            record={}
+            series=[]
+
+            for item in self.sse_source:
+                received_record=json.loads(item.data)
+
+                Id=received_record['round_id']
+                multiplier=received_record['multiplier']
+                std_time=received_record['std_time']
+                unix_time=received_record['unix_time']
+
+                if multiplier>target_multiplier:
+                    Close+=target_multiplier-1
                 else:
-                    Coeff-=1
                     Close-=1
-            else:
-                Close=Coeff+round(random.uniform(-0.5,0.5),2)
-                
-            High=max(Open,High,Close)
-            Low=min(Open,Low,Close)
-            self.record={'std_time':std_time,'unix_time':unix_time,'multiplier':multiplier,'coef':Coeff,'open':round(Open,2),'high':round(High,2),'low':round(Low,2),'close':round(Close,2)}
-            
-            if self.timer():
-                unix_time=datetime.now().timestamp()
-                Open=Close
-                High=float('-inf')
-                Low=float('inf')
-                
-                self.series.append(self.record)
-                
-            time.sleep(1)
-    def expose(self):
-        def serve_series():
-            EPoint=f"series/{time_frame['name']}/{token['name']}"
-            url=f"/market/{EPoint}"
 
-            app.add_url_rule(url,endpoint=EPoint,view_func=lambda:jsonify(self.series))
-            
-        def serve_latest():
-            EPoint=f"latest/{time_frame['name']}/{token['name']}"
-            url=f"/market/{EPoint}"
+                High=max(Open,High,Close)
+                Low=min(Open,Low,Close)
+                record={'id':Id,'interval_time':interval_time,'std_time':std_time,'unix_time':unix_time,'multiplier':multiplier,'open':round(Open,2),'high':round(High,2),'low':round(Low,2),'close':round(Close,2)}
 
-            app.add_url_rule(url,endpoint=EPoint,view_func=lambda:jsonify(self.record))
+                if timer(time_frame):
+                    interval_time=std_time
+                    Open=Close
+                    High=float('-inf')
+                    Low=float('inf')
+                    
+                    series.append(record)
+                    print(f'{c}reset has occured!')
+
+                print(f'{b}round_id:{Id} | time:{std_time} | multiplier:{multiplier} | open:{Open} | high:{High} | low:{Low} | close:{Close}')
+                time.sleep(1)
+
+            return record,series
         
-        def serve_stream():
-            EPoint=f"stream/{time_frame['name']}/{token['name']}"
-            url=f"/market/{EPoint}"
-            app.add_url_rule(url,endpoint=EPoint,view_func=lambda:Response(self.generator(),mimetype='text/event-stream'))
+        threading.Thread(target=generate_metrics,daemon=True).start()
 
-        app.run(debug=True,host='0.0.0.0',port=8000)
+start_program_at_0sec()
+transformer=Transformer(f'http://{ip_address}:8080/mozzart_aviator/stream')
+transformer.connect()
+transformer.quantifier(5,'minute_10')
 
-with open('config/format.json','r') as file:
-    format_data=json.load(file)
-
-time_frames=format_data['time_frames']
-tokens=format_data['tokens']
-
-for time_frame in time_frames:
-    for token in tokens:
-        transformer=Transformer('http://127.0.0.1:8080/raw/stream',token,time_frame)
-        transformer.start()
-        transformer.expose()
+main_thread()
