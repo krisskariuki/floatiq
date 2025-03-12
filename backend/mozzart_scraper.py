fb1bb352350264f82ee8e3d6bc0d1e2bf855f42c
kriss <chri570ph3rk4riuki@gmail.com>
2025-03-12 18:36:08 +0300

    increased sleep time in the last click action to 3seconds

--- backend/mozzart_scraper.py
+++ backend/mozzart_scraper.py
@@ -19,7 +19,7 @@ scraper.base_file_name='mozzart_aviator'
 scraper.action(action='click',attribute='class="login-link mozzart_ke"',message='logging in...')
 scraper.action(action='write',attribute='placeholder="Mobile number"',message='writing phone input...',input_value=PHONE)
 scraper.action(action='send',attribute='placeholder="Password"',message='writing password input...',input_value=PASSWORD)
-scraper.action(action='click',attribute='alt="Aviator"',message='connecting to game engine...',sleep_time=1)
+scraper.action(action='click',attribute='alt="Aviator"',message='connecting to game engine...',sleep_time=3)
 scraper.action(action='callback',callback=scraper.watch_aviator)
 
 scraper.navigate()
