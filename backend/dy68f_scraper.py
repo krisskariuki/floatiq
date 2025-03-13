cc5b0347c89f4042b23559e88b8983a094c30d11
kriss <chri570ph3rk4riuki@gmail.com>
2025-03-13 15:37:49 +0300

    minor patches

---
+++ backend/dy68f_scraper.py
@@ -0,0 +1,21 @@
+from scraper import Scraper
+from config import DY68F_URL
+from credentials import DY68F_EMAIL,DY68F_PASSWORD
+import argparse
+
+parser=argparse.ArgumentParser(description='for controlling the bots configuration parameters')
+parser.add_argument('--headless',action='store_true')
+parser_arguments=parser.parse_args()
+
+dy68f_scrapper=Scraper(target_url=DY68F_URL,wait_time=30,headless=parser_arguments.headless)
+
+dy68f_scrapper.action(action='write',attribute='type="text"',input_value=DY68F_EMAIL,message='phone input...')
+dy68f_scrapper.action(action='send',attribute='type="password"',input_value=DY68F_PASSWORD,message='password input...')
+dy68f_scrapper.action(action='click',attribute='class="loginin btncolor"',message='going to market list...')
+dy68f_scrapper.action(action='click_from_list',attribute='class="list-item"',message='choosing game...')
+dy68f_scrapper.action(action='click_from_list',attribute='class="btns"',message='selecting project...')
+dy68f_scrapper.action(action='click',attribute='class="btns"',message='placing amount...')
+dy68f_scrapper.action(action='click',attribute='class="confirms btncolor"',message='placing trade order...')
+
+
+dy68f_scrapper.navigate()
