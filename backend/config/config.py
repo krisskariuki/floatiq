bca791af5a5b4d8c3da995c93cfb7b493b76378f
kriss <chri570ph3rk4riuki@gmail.com>
2025-03-13 06:40:24 +0300

    -relocated commonly used variables  & some important configurations to this one file.\n-They are designed to be accessible & modifiable via the terminal as environment variables

--- backend/config/config.py
+++ backend/config/config.py
@@ -1,2 +1,14 @@
-TARGET_MULTIPLIERS=['two_2','three_3','five_5','ten_10','twenty_20','sixty_60']
-TIME_FRAMES=['minute_10','minute_30','hour_1','hour_4','hour_6','hour_12','day_1']
+from dotenv import load_dotenv
+import os
+import socket
+
+load_dotenv()
+
+MOZZART_URL=os.getenv('MOZZART_URL','https://www.mozzartbet.co.ke/en#/casino')
+
+TARGET_MULTIPLIERS=os.getenv('TARGET_MULTIPLIERS',"two_2,three_3,five_5,ten_10,twenty_20,sixty_60").split(',')
+TIME_FRAMES=os.getenv('TIME_FRAMES',"minute_10,minute_30,hour_1,hour_4,hour_6,hour_12,day_1").split(',')
+
+LOCAL_IP=os.getenv('LOCAL_IP',socket.gethostbyname(socket.gethostname()))
+PRODUCER_PORT=int(os.getenv('EVENT_PRODUCER_PORT',8080))
+PROCESSOR_PORT=int(os.getenv('EVENT_PROCESSOR_PORT',8000))
