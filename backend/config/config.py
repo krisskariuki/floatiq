10cf372661282e5729065b1fed25693000e4df99
kriss <chri570ph3rk4riuki@gmail.com>
2025-03-13 10:25:26 +0300

    removed unneccessary characters for each item in TARGET_MULTIPLIERS

--- backend/config/config.py
+++ backend/config/config.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 MOZZART_URL=os.getenv('MOZZART_URL','https://www.mozzartbet.co.ke/en#/casino')
 
-TARGET_MULTIPLIERS=os.getenv('TARGET_MULTIPLIERS',"two_2,three_3,five_5,ten_10,twenty_20,sixty_60").split(',')
+TARGET_MULTIPLIERS=os.getenv('TARGET_MULTIPLIERS',"2.00,3.00,5.00,10.00").split(',')
 TIME_FRAMES=os.getenv('TIME_FRAMES',"minute_10,minute_30,hour_1,hour_4,hour_6,hour_12,day_1").split(',')
 
 LOCAL_IP=os.getenv('LOCAL_IP',socket.gethostbyname(socket.gethostname()))
