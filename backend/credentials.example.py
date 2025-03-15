2d02b809603ad174e7cb3c2270fb1ffa8fb744de
kriss <chri570ph3rk4riuki@gmail.com>
2025-03-16 00:13:34 +0300

    an example file for setting up credentials

---
+++ backend/credentials.example.py
@@ -0,0 +1,7 @@
+#replace the values with actual credentials and rename this file to "credentials.py" for the program you're running to work properly
+
+MOZZART_PHONE='0133976734'
+MOZZART_PASSWORD='Passwrd234_moz.'
+
+DY68F_EMAIL='youremail@gmail.com'
+DY68_PASSWORD='Passwrd123_dy68.'
