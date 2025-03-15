bf720dc35e2ec7033102534ed33cc5bbca4f5947
kriss <chri570ph3rk4riuki@gmail.com>
2025-03-15 10:53:07 +0300

    added 'minute_5' to the TARGET_MULTIPLIERS

--- backend/config/config.py
+++ backend/config/config.py
@@ -8,7 +8,7 @@ MOZZART_URL=os.getenv('MOZZART_URL','https://www.mozzartbet.co.ke/en#/casino')
 DY68F_URL=os.getenv('DY68F','https://a.dy68fo0tball.com/#/pages/login/index')
 
 TARGET_MULTIPLIERS=os.getenv('TARGET_MULTIPLIERS',"2.00,3.00,5.00,10.00").split(',')
-TIME_FRAMES=os.getenv('TIME_FRAMES',"minute_10,minute_30,hour_1,hour_4,hour_6,hour_12,day_1").split(',')
+TIME_FRAMES=os.getenv('TIME_FRAMES',"minute_5,minute_10,minute_30,hour_1,hour_4,hour_6,hour_12,day_1").split(',')
 
 LOCAL_IP=os.getenv('LOCAL_IP',socket.gethostbyname(socket.gethostname()))
 PRODUCER_PORT=int(os.getenv('EVENT_PRODUCER_PORT',8080))
