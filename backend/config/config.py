f5a8fa089084b294688aa510f8a7e2e5ca441859
kriss <chri570ph3rk4riuki@gmail.com>
2025-03-16 08:53:46 +0300

    added a PRODUCER_STREAM variable for setting the streaming sse_url source dymanically

--- backend/config/config.py
+++ backend/config/config.py
@@ -13,3 +13,4 @@ TIME_FRAMES=os.getenv('TIME_FRAMES',"minute_5,minute_10,minute_30,hour_1,hour_4,
 LOCAL_IP=os.getenv('LOCAL_IP',socket.gethostbyname(socket.gethostname()))
 PRODUCER_PORT=int(os.getenv('EVENT_PRODUCER_PORT',8080))
 PROCESSOR_PORT=int(os.getenv('EVENT_PROCESSOR_PORT',8000))
+PRODUCER_STREAM=os.getenv('PRODUCER_STREAM','simulator/stream')
