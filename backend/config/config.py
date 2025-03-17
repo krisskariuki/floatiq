0b2a2120c74809bb2c9f3ba5ec7926cbd33826ab
kriss <chri570ph3rk4riuki@gmail.com>
2025-03-17 21:49:23 +0300

    added more target multipliers

--- backend/config/config.py
+++ backend/config/config.py
@@ -7,7 +7,7 @@ load_dotenv()
 MOZZART_URL=os.getenv('MOZZART_URL','https://www.mozzartbet.co.ke/en#/casino')
 DY68F_URL=os.getenv('DY68F','https://a.dy68fo0tball.com/#/pages/login/index')
 
-TARGET_MULTIPLIERS=os.getenv('TARGET_MULTIPLIERS',"2.00,3.00,5.00,10.00").split(',')
+TARGET_MULTIPLIERS=os.getenv('TARGET_MULTIPLIERS',"2.00,3.00,5.00,10.00,15.00,25.00,30.00,45.00,50.00,65.00,75.00").split(',')
 TIME_FRAMES=os.getenv('TIME_FRAMES',"minute_5,minute_10,minute_30,hour_1,hour_4,hour_6,hour_12,day_1").split(',')
 
 LOCAL_IP=os.getenv('LOCAL_IP',socket.gethostbyname(socket.gethostname()))
