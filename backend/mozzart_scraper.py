0aca74092309082dc49c2c278266ec9764739b16
kriss <chri570ph3rk4riuki@gmail.com>
2025-03-18 22:09:45 +0300

    added account credentials for a master account

--- backend/mozzart_scraper.py
+++ backend/mozzart_scraper.py
@@ -1,7 +1,7 @@
 from scraper import Scraper
 from config import MOZZART_URL
 from utils import main_thread
-from credentials import MOZZART_PHONE,MOZZART_PASSWORD
+from credentials import MOZZART_MASTER_PHONE,MOZZART_MASTER_PASSWORD
 import os
 import argparse
 
@@ -15,8 +15,8 @@ scraper.folder_name='backup'
 scraper.base_file_name='mozzart_aviator'
 
 scraper.action(action='click',attribute='class="login-link mozzart_ke"',message='logging in...')
-scraper.action(action='write',attribute='placeholder="Mobile number"',message='writing phone input...',input_value=MOZZART_PHONE)
-scraper.action(action='send',attribute='placeholder="Password"',message='writing password input...',input_value=MOZZART_PASSWORD)
+scraper.action(action='write',attribute='placeholder="Mobile number"',message='writing phone input...',input_value=MOZZART_MASTER_PHONE)
+scraper.action(action='send',attribute='placeholder="Password"',message='writing password input...',input_value=MOZZART_MASTER_PASSWORD)
 scraper.action(action='click',attribute='alt="Aviator"',message='connecting to game engine...',sleep_time=3)
 scraper.action(action='callback',callback=scraper.watch_aviator)
 
