1db99ccc26ddc0027851355b9f69c7fdb779eaf9
kriss <chri570ph3rk4riuki@gmail.com>
2025-03-23 17:21:46 +0300

    added command line arguments for phone and password

--- backend/mozzart_scraper.py
+++ backend/mozzart_scraper.py
@@ -8,6 +8,9 @@ import argparse
 parser=argparse.ArgumentParser(description='for controlling the bots configuration parameters')
 parser.add_argument('--headless',action='store_true')
 parser.add_argument('--backup',action='store_true')
+parser.add_argument('--phone',default=MOZZART_MASTER_PHONE)
+parser.add_argument('--password',default=MOZZART_MASTER_PASSWORD)
+
 parser_arguments=parser.parse_args()
 
 scraper=Scraper(target_url=MOZZART_URL,wait_time=30,headless=parser_arguments.headless,backup=parser_arguments.backup)
@@ -16,8 +19,8 @@ scraper.folder_name='backup'
 scraper.base_file_name='mozzart_aviator'
 
 scraper.action(action='click',attribute='class="login-link mozzart_ke"',message='logging in...')
-scraper.action(action='write',attribute='placeholder="Mobile number"',message='writing phone input...',input_value=MOZZART_MASTER_PHONE)
-scraper.action(action='send',attribute='placeholder="Password"',message='writing password input...',input_value=MOZZART_MASTER_PASSWORD)
+scraper.action(action='write',attribute='placeholder="Mobile number"',message='writing phone input...',input_value=parser_arguments.phone)
+scraper.action(action='send',attribute='placeholder="Password"',message='writing password input...',input_value=parser_arguments.password)
 scraper.action(action='click',attribute='alt="Aviator"',message='connecting to game engine...',sleep_time=3)
 scraper.action(action='callback',callback=scraper.watch_aviator)
 
